79115dabcde606d13a5a577e5076bee3ab53f648
Wallet <joseouellet@gmail.com>
2022-12-31 01:41:09 -0500

    removed fr

--- main.go
+++ main.go
@@ -19,36 +19,4 @@ func main() {
 	go server.StartServer()
 
 	a.Run()
-
-
-	// fs := http.FileServer(http.Dir("./frontend/web-build"))
-	// http.Handle("/", fs)
-
-	// openbrowser("http://localhost:3000")
-	
-	// log.Print("Listening on :3000...")
-	// err := http.ListenAndServe(":3000", nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 }
-
-// func openbrowser(url string) {
-// 	var err error
-
-// 	switch runtime.GOOS {
-// 	case "linux":
-// 		err = exec.Command("xdg-open", url).Start()
-// 	case "windows":
-// 		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-// 	case "darwin":
-// 		err = exec.Command("open", url).Start()
-// 	default:
-// 		err = fmt.Errorf("unsupported platform")
-// 	}
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// }
